45a4d2ffa7a93a9d07a451ba6ca4ca589ed831ed
james-mbuvi <james.mbuvi@student.moringaschool.com>
2024-03-02 13:53:59 +0300

    completed assignment

--- lib/testing/dog.py
+++ lib/testing/dog.py
@@ -1,50 +1,4 @@
-
-# class Dog:
-#     APPROVED_BREEDS = [
-#         "Mastiff",
-#         "Chihuahua",
-#         "Corgi",
-#         "Shar Pei",
-#         "Beagle",
-#         "French Bulldog",
-#         "Pug",
-#         "Pointer"
-#     ]
-
-#     def __init__(self, name="Dog", breed="Unknown"):
-#         self.name = name
-#         self.breed = breed
-
-#     @property
-#     def name(self):
-#         return self._name
-
-#     @name.setter
-#     def name(self, value):
-#         if not isinstance(value, str) or not (1 <= len(value) <= 25):
-#             raise ValueError("Name must be a string between 1 and 25 characters.")
-#         else:
-#             self._name = value
-
-#     @property
-#     def breed(self):
-#         return self._breed
-
-#     @breed.setter
-#     def breed(self, value):
-#         if value not in self.APPROVED_BREEDS:
-#             raise ValueError("Breed must be in list of approved breeds.")
-#         else:
-#             self._breed = value
-
-#             pass
-
-
-
-
-
-
-
+#!/usr/bin/env python3
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -57,7 +11,6 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-
 class Dog:
     def __init__(self, name='Fido', breed='Mastiff'):
         self.name = name
@@ -65,30 +18,25 @@ class Dog:
 
     def get_name(self):
         return self._name
-
+    
     def set_name(self, name):
         if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+            self._name = name
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            print("Name must be string between 1 and 25 characters.")
 
     name = property(get_name, set_name)
 
     def get_breed(self):
         return self._breed
-
+    
     def set_breed(self, breed):
         if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
-            raise ValueError("Breed must be in list of approved breeds.")
+            print("Breed must be in list of approved breeds.")
 
     breed = property(get_breed, set_breed)
-
-
-
-
 
 
 
--- lib/testing/person.py
+++ lib/testing/person.py
@@ -16,35 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-
-    def __init__(self, name='Fido', breed='Mastiff'):
+    def __init__(self, name='J. Doe', job='Sales'):
         self.name = name
-        self.breed = breed
+        self.job = job
 
     def get_name(self):
         return self._name
-
+    
     def set_name(self, name):
         if isinstance(name, str) and 1 <= len(name) <= 25:
             self._name = name.title()
         else:
-            raise print(
-                "Name must be string between 1 and 25 characters.")
+            print("Name must be string between 1 and 25 characters.")
 
     name = property(get_name, set_name)
 
-    def get_breed(self):
-        return self._breed
-
-    def set_breed(self, breed):
-        if breed in APPROVED_JOBS:
-            self._breed = breed
-        else:
-            raise print("Breed must be in list of approved jobs.")
-
-    breed = property(get_breed, set_breed)
-
-
+    def get_job(self):
+        return self._job
     
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
 
-    pass
+    job = property(get_job, set_job)
